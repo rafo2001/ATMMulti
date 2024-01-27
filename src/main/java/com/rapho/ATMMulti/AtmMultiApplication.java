3e334648fe15204de664ebde9e3c07edc6a7fef5
rafo2001 <raphael.munoz@gmail.com>
2024-01-26 23:22:49 -0400

    microservicio tasaCambio
    http://localhost:8080/tasaCambio

--- src/main/java/com/rapho/ATMMulti/AtmMultiApplication.java
+++ src/main/java/com/rapho/ATMMulti/AtmMultiApplication.java
@@ -2,9 +2,17 @@ package com.rapho.ATMMulti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@RestController
 @SpringBootApplication
 public class AtmMultiApplication {
+
+	@GetMapping("tasaCambio")
+	public String getMessage() {
+		return "6.97";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtmMultiApplication.class, args);
